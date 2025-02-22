6e097725c17cd3336f39699a060f5ceb5398909e
shahalimshompod <shahalimsompod@gmail.com>
2025-02-22 17:52:40 +0600

    added some features and deploy database to vercel

--- src/hooks/useSocket.jsx
+++ src/hooks/useSocket.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { io } from "socket.io-client";
 
 // Socket instance
-const socket = io("http://localhost:5000");
+const socket = io("https://getitdone-xi.vercel.app");
 
 const useSocket = (event, callback) => {
   useEffect(() => {
